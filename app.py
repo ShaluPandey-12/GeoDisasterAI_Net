00a9056fc60e663a51431f77113b4707f96df231
Sherni12 <shalupandey129247@gmail.com>
2026-05-10 21:59:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -247,7 +247,5 @@ def about3():
 def about4():
     return render_template("Notebook3.html")
 
-
-
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False)
